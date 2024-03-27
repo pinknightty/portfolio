13ac2d44e0b87a9681fd1398166bf95a2c46a3ca
pinknightty <rafagrosa@gmail.com>
2024-03-27 17:20:00 -0300

    Finalização do projeto, com alguns bugs ainda:
    * Tag Aside não está centralizando
    * Não consegui fazer o botão Light/Dark continuar da cor de antes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
+import { ThemeProvider } from 'styled-components'
+import { useState } from 'react'
+
 import Projetos from './containers/Projetos'
 import Sidebar from './containers/Sidebar'
 import Sobre from './containers/Sobre'
 import EstiloGlobal, { Container } from './styles'
+import temaLight from './themes/light'
+import temaDark from './themes/dark'
 
 function App() {
+  const [isDark, setIsDark] = useState(true)
+
+  function trocaTema() {
+    setIsDark(!isDark)
+  }
+
   return (
-    <>
+    <ThemeProvider theme={isDark ? temaDark : temaLight}>
       <EstiloGlobal />
       <Container>
-        <Sidebar />
+        <Sidebar trocaTema={trocaTema} />
         <main>
           <Sobre />
           <Projetos />
         </main>
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 

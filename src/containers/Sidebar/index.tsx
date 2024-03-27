8a11c658ccc1d2fef0941c2af11faea20717cdd9
pinknightty <rafagrosa@gmail.com>
2024-03-27 15:32:59 -0300

    Estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/pinknightty.png" />
-    <Titulo fontSize={20}>Rafael Rosa</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Rafael Rosa</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        pinknightty
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Dev Front-End Jr
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
